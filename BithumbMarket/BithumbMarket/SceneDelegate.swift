98a730e5d3a597fc21b0d63adbd2556076b8f520
HOONHA CHOI <sosqsz2@naver.com>
2022-02-23 10:24:55 +0900

    initial commit

---
+++ BithumbMarket/BithumbMarket/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  BithumbMarket
+//
+//  Created by HOONHA CHOI on 2022/02/23.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
+
