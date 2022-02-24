bda5dfb99ce1e4cd118b5454520c10e9bea21e91
JIINHEO <gjwldls123@gmail.com>
2022-02-24 11:03:13 +0900

    [#4] feat: Transaction ViewController 추가

---
+++ BithumbMarket/BithumbMarket/Transaction/TransactionViewController.swift
@@ -0,0 +1,12 @@
+//
+//  TransactionViewController.swift
+//  BithumbMarket
+//
+//  Created by jiinheo on 2022/02/24.
+//
+
+import UIKit
+
+class TransactionViewController: ViewController {
+    
+}
