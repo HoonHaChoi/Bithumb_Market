48c886d917b266dbffa72b8a43cfc81c196662dc
JIINHEO <gjwldls123@gmail.com>
2022-02-24 12:03:17 +0900

    [#4] feat: TableView 구현

--- BithumbMarket/BithumbMarket/Transaction/TransactionViewController.swift
+++ BithumbMarket/BithumbMarket/Transaction/TransactionViewController.swift
@@ -8,5 +8,53 @@
 import UIKit
 
 class TransactionViewController: ViewController {
+
+    let row = 20
+    
+    private lazy var tableView: UITableView = {
+        let view = UITableView()
+        view.delegate = self
+        view.dataSource = self
+        view.register(UITableViewCell.self, forCellReuseIdentifier: "TransactionTableViewCell")
+        return view
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+}
+
+extension TransactionViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return row
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TransactionTableViewCell", for: indexPath) as UITableViewCell
+        cell.textLabel?.text = "title"
+        
+        return cell
+    }
+}
+
+extension TransactionViewController: UITableViewDelegate {
     
 }
+
+extension TransactionViewController {
+    
+    func setupView() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+    
+}
+
