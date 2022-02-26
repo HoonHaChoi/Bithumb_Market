ecf096890d3ba9f986585f93b80b49597d56054f
Doku <ido.young@icloud.com>
2022-02-26 10:32:09 +0900

    [#3] feat: OrderbookViewModel 바인딩
    바인딩에 필요한 프로퍼티 생성

--- BithumbMarket/BithumbMarket/OrderbookViewModel/OrderbookViewModel.swift
+++ BithumbMarket/BithumbMarket/OrderbookViewModel/OrderbookViewModel.swift
@@ -9,7 +9,22 @@ import Foundation
 
 class OrderbookViewModel {
 
-    var orderbookData: OrderbookData?
+    //TODO: 뷰와 의존 관계에 따라 변경 및 추가
+    var binding: () -> Void = { }
+    
+    private var orderbookData: OrderbookData? {
+        didSet {
+            binding()
+        }
+    }
+    
+    //TODO: TableViewDataSource에 따라 필요없는 프로퍼티 제거 및 추가
+    var asksPrice: [String]?
+    var asksQuanity: [String]?
+    var bidsPrice: [String]?
+    var bidsQuanity: [String]?
+    
+    //TODO: Quanity 연산 작업
     
     private let url = EndPoint().makeURL(of: .orderBook, param: "BTC_KRW")
     
@@ -18,7 +33,11 @@ class OrderbookViewModel {
             switch result {
             case .success(let success):
                 self?.orderbookData = success.data
+                self?.asksPrice = self?.orderbookData?.asks.map{ $0.quantity }
+                self?.bidsPrice = self?.orderbookData?.asks.map{ $0.quantity }
+                self?.bidsQuanity = self?.orderbookData?.bids.map{ $0.price }
             case .failure(let failure):
+                //TODO: fail 처리
                 print(failure)
             }
         }
