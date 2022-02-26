d09dbfd59ad624b633563d4eb92cdd052a3f8ada
HOONHA CHOI <sosqsz2@naver.com>
2022-02-27 03:16:05 +0900

    [#9] refactor: Endpoint 프로토콜 변경

--- BithumbMarket/BithumbMarket/Network/EndPoint.swift
+++ BithumbMarket/BithumbMarket/Network/EndPoint.swift
@@ -7,28 +7,13 @@
 
 import Foundation
 
-struct EndPoint {
-    
-    private let scheme = "https"
-    private let host = "api.bithumb.com"
-    private let path = "/public/"
-    
-    enum RequestType: String {
-        case ticker = "ticker/"
-        case orderBook = "orderbook/"
-        case transactionHistory = "transaction_history/"
-        case assetsStatus = "assetsstatus/"
-        
-        var name: String {
-            return self.rawValue
-        }
-    }
-    
-    func makeURL(of type: RequestType, param: String) -> URL? {
-        var component = URLComponents()
-        component.scheme = scheme
-        component.host = host
-        component.path = path + type.name + param
-        return component.url
+protocol Endpoint {
+    var baseURLString: String { get }
+    var path: String { get }
+}
+
+extension Endpoint {
+    var url: URL? {
+        return URL(string: baseURLString + path)
     }
 }
