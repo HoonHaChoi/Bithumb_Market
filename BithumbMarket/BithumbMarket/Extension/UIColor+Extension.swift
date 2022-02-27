ccc89d54d8e849d00ce68704f7523fa40f31a5a5
HOONHA CHOI <sosqsz2@naver.com>
2022-02-28 03:13:54 +0900

    [#10] feat: UIColor+Extension 추가 및 컬러 프로퍼티 구현

---
+++ BithumbMarket/BithumbMarket/Extension/UIColor+Extension.swift
@@ -0,0 +1,57 @@
+//
+//  UIColor+Extension.swift
+//  BithumbMarket
+//
+//  Created by HOONHA CHOI on 2022/02/28.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static var actionBackgroundPrimary: UIColor {
+        .init(named: "ActionBackgroundPrimary") ?? .init()
+    }
+    
+    static var actionTextPrimary: UIColor {
+        .init(named: "ActionTextPrimary") ?? .init()
+    }
+    
+    static var actionTextSecondary: UIColor {
+        .init(named: "ActionTextSecondary") ?? .init()
+    }
+    
+    static var fallColor: UIColor {
+        .init(named: "FallColor") ?? .init()
+    }
+    
+    static var mainColor: UIColor {
+        .init(named: "MainColor") ?? .init()
+    }
+    
+    static var orderBookBuyBackground: UIColor {
+        .init(named: "orderBookBuyBackground") ?? .init()
+    }
+    
+    static var orderBookSellBackground: UIColor {
+        .init(named: "OrderBookSellBackground") ?? .init()
+    }
+    
+    static var riseColor: UIColor {
+        .init(named: "RiseColor") ?? .init()
+    }
+    
+    static var textPrimary: UIColor {
+        .init(named: "TextPrimary") ?? .init()
+    }
+    
+    static var textSecondary: UIColor {
+        .init(named: "TextSecondary") ?? .init()
+    }
+    
+    static var typoColor: UIColor {
+        .init(named: "TypoColor") ?? .init()
+    }
+    
+}
+
