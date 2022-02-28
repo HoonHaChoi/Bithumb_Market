c0173320bb4c4f4f53fe6b70aca08eaf8dd926d8
Doku <ido.young@icloud.com>
2022-02-28 09:43:57 +0900

    [#3] refactor: init() 수정 및 Orderbook 모델 뷰모델에 적용

--- BithumbMarket/BithumbMarket/OrderbookViewModel/OrderbookViewModel.swift
+++ BithumbMarket/BithumbMarket/OrderbookViewModel/OrderbookViewModel.swift
@@ -7,34 +7,35 @@
 
 import Foundation
 
-//TODO: ViewModel 타입 불필요시 제거
 protocol OrderbookViewModelType {
-    var binding: () -> Void { get set}
     
-    var priceOfAsks: [String]? { get }
-    var quantityOfAsks: [String]? { get }
-    var priceOfBids: [String]? { get }
-    var quantityOfBids: [String]? { get }
+    var binding: () -> Void { get set}
+    var orderbook: Orderbook { get }
+    func request(completion: @escaping () -> Void)
+    
 }
 
 class OrderbookViewModel: OrderbookViewModelType {
 
     //TODO: 뷰와 의존 관계에 따라 변경 및 추가
     var binding: () -> Void = { }
-    
-    private var orderbookData: OrderbookData? {
+    var orderbook: Orderbook {
         didSet {
             binding()
         }
     }
+    private var service: APIService
     
-    //TODO: TableViewDataSource에 따라 필요없는 프로퍼티 제거 및 추가
-    var priceOfAsks: [String]?
-    var quantityOfAsks: [String]?
-    var priceOfBids: [String]?
-    var quantityOfBids: [String]?
+    init(service: APIService) {
+        self.service = service
+        self.orderbook = Orderbook(
+            asksPrice: Array(repeating: "", count: 30),
+            bidsPrice: Array(repeating: "", count: 30),
+            asksQuanity: Array(repeating: Quantity(text: "", rate: 0), count: 30),
+            bidsQuanity: Array(repeating: Quantity(text: "", rate: 0), count: 30)
+        )
+    }
     
-    //TODO: 수정
     private func calculateRateOfQuintity(quantities: [String], quantity: String) -> Float {
         let quantities = quantities.map { Float($0) ?? 0 }
         let sumOfQuantities = quantities.reduce(0){ $0 + $1}
@@ -43,15 +44,36 @@ class OrderbookViewModel: OrderbookViewModelType {
     
     private let url = EndPoint().makeURL(of: .orderBook, param: "BTC_KRW")
     
-    init(service: APIService) {
+    //TODO: 메소드 분리
+    func request(completion: @escaping () -> Void) {
         service.request(url: url) { [weak self] (result: Result<OrderbookEntity, HTTPError>) in
             switch result {
             case .success(let success):
-                self?.orderbookData = success.data
-                self?.priceOfAsks = self?.orderbookData?.asks.map{ $0.price }
-                self?.quantityOfAsks = self?.orderbookData?.asks.map{ $0.quantity }
-                self?.priceOfBids = self?.orderbookData?.bids.map{ $0.price }
-                self?.quantityOfBids = self?.orderbookData?.asks.map{ $0.quantity }
+                let orderbookData = success.data
+                self?.orderbook = Orderbook(
+                    asksPrice: orderbookData.asks.reversed().map { $0.price },
+                    bidsPrice: orderbookData.bids.reversed().map { $0.price }.reversed(),
+                    asksQuanity:  orderbookData.bids.reversed()
+                        .map {
+                            Quantity(
+                                text: $0.quantity,
+                                rate: self?.calculateRateOfQuintity(
+                                    quantities: orderbookData.asks.map{ $0.quantity },
+                                    quantity: $0.quantity
+                                ) ?? 0
+                            )
+                        },
+                    bidsQuanity: orderbookData.asks.reversed()
+                        .map {
+                            Quantity(
+                                text: $0.quantity,
+                                rate: self?.calculateRateOfQuintity(
+                                    quantities: orderbookData.bids.map{ $0.quantity },
+                                    quantity: $0.quantity
+                                ) ?? 0
+                            )
+                        }
+                )
             case .failure(let failure):
                 //TODO: fail 처리
                 print(failure)
