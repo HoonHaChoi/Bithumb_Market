9e87e4874ba83ab9ba9712d8b341d88bb3784cf4
HOONHA CHOI <sosqsz2@naver.com>
2022-02-28 16:35:20 +0900

    [#14] feat: MainViewModel 생성자 추가, 목록가져오기

--- BithumbMarket/BithumbMarket/View/Main/MainViewModel.swift
+++ BithumbMarket/BithumbMarket/View/Main/MainViewModel.swift
@@ -8,5 +8,30 @@
 import Foundation
 
 final class MainViewModel {
-
+    
+    var tickers: Observable<[Ticker]>
+    private var service: APIService
+    
+    init(service: APIService = APIService()) {
+        tickers = .init([])
+        self.service = service
+    }
+    
+    var updateTableHandler: (() -> Void)?
+    var errorHandler: ((HTTPError) -> Void)?
+    
+    func fetchTickers() {
+        service.requestTickers(endpoint: .ticker()) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let tickers):
+                self.tickers.value = tickers
+                self.updateTableHandler?()
+                
+            case .failure(let error):
+                self.errorHandler?(error)
+            }
+        }
+    }
+    
 }
