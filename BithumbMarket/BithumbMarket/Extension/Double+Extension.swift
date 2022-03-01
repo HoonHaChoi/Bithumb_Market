89f8a692125f37a8e3aef3011514e94a0b1bf8df
HOONHA CHOI <sosqsz2@naver.com>
2022-03-02 02:59:26 +0900

    [#18] feat: 거래금액 format 추가

---
+++ BithumbMarket/BithumbMarket/Extension/Double+Extension.swift
@@ -0,0 +1,25 @@
+//
+//  Double+Extension.swift
+//  BithumbMarket
+//
+//  Created by HOONHA CHOI on 2022/03/02.
+//
+
+import Foundation
+
+extension Double {
+        
+    var formatPrice: String {
+        let unit = ["백만", "십만", "만", "천", "원"]
+        var standardValue: Double = 1000000
+        
+        for i in 0..<unit.count {
+            if self/standardValue > 1 {
+                return String(self/standardValue).withDecimal(maximumDigit: 0) + unit[i]
+            }
+            standardValue /= 10
+        }
+        return String(self)
+    }
+    
+}
