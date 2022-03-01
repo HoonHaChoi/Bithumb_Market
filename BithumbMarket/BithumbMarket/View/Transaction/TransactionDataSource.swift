d7281caf121ed196dd0fd83cdcc35ec4c99e9fff
JIINHEO <gjwldls123@gmail.com>
2022-03-01 14:41:17 +0900

    [#15] feat: TransactionDataSource 추가

---
+++ BithumbMarket/BithumbMarket/View/Transaction/TransactionDataSource.swift
@@ -0,0 +1,27 @@
+//
+//  TransactionDataSource.swift
+//  BithumbMarket
+//
+//  Created by jiinheo on 2022/03/01.
+//
+
+import UIKit
+
+final class TransactionDataSource: NSObject, UITableViewDataSource {
+    
+    var items: [TransactionData]
+    
+    override init() {
+        items = .init()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TransactionNameSpace.cellReuseIdentifier, for: indexPath) as? TransactionTableViewCell else { return .init()}
+        cell.configure(transaction: items[indexPath.row])
+        return cell
+    }
+}
