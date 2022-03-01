47cfa7b6dc2f93d8949d25df2ded45805e950331
Doku <ido.young@icloud.com>
2022-03-01 23:52:13 +0900

    [#13] refactor: REST API  Orderbook 요청 메소드 수정

--- BithumbMarket/BithumbMarket/View/Orderbook/OrderbookViewModel/OrderbookViewModel.swift
+++ BithumbMarket/BithumbMarket/View/Orderbook/OrderbookViewModel/OrderbookViewModel.swift
@@ -8,19 +8,18 @@
 import Foundation
 
 protocol OrderbookViewModelType {
-    
     var orderbook: Observable<Orderbook> { get }
-    func request(completion: @escaping () -> Void)
-    
+    var errorHandler: ((HTTPError) -> Void)? { get set }
+    func featchOrderbook()
 }
 
 final class OrderbookViewModel: OrderbookViewModelType {
 
+    var symbol: String
     var orderbook: Observable<Orderbook>
     private var service: APIService
     
-    init(service: APIService) {
-        self.service = service
+    init(service: APIService, symbol: String) {
         self.orderbook =  Observable(
             Orderbook(
                 asks: Array(
@@ -37,51 +36,44 @@ final class OrderbookViewModel: OrderbookViewModelType {
                     count: 30)
             )
         )
+        self.service = service
+        self.symbol = symbol
+    }
+    
+    var errorHandler: ((HTTPError) -> Void)?
+    
+    func featchOrderbook() {
+        service.request(endpoint: .orderBook(symbol: symbol)) { [weak self] (result: Result<OrderbookEntity, HTTPError>) in
+            switch result {
+            case .success(let success):
+                let orderbook = success.data
+                guard let asks = self?.convert(from: orderbook.asks),
+                      let bids = self?.convert(from: orderbook.bids) else {
+                          return
+                      }
+                self?.orderbook.value = Orderbook(asks: asks, bids: bids)
+            case .failure(let error):
+                self?.errorHandler?(error)
+            }
+        }
+    }
+    
+    private func convert(from entity: [OrderEntity]) -> [Order]{
+        return entity
+            .map {
+                Order(
+                    price: $0.price,
+                    quantity: $0.quantity,
+                    rateOfQuantity: self.calculateRateOfQuintity(
+                        quantities: entity.map{ $0.quantity },
+                        quantity: $0.quantity))
+            }
     }
     
     private func calculateRateOfQuintity(quantities: [String], quantity: String) -> Float {
         let quantities = quantities.map { Float($0) ?? 0 }
         let sumOfQuantities = quantities.reduce(0){ $0 + $1}
         return Float(quantity) ?? 0 / sumOfQuantities * 5
-    }
-    
-    private let url = EndPoint().makeURL(of: .orderBook, param: "BTC_KRW")
-    
-    //TODO: 메소드 분리
-    func request(completion: @escaping () -> Void) {
-        service.request(url: url) { [weak self] (result: Result<OrderbookEntity, HTTPError>) in
-            switch result {
-            case .success(let success):
-                let orderbookData = success.data
-                self?.orderbook = Orderbook(
-                    asksPrice: orderbookData.asks.reversed().map { $0.price },
-                    bidsPrice: orderbookData.bids.reversed().map { $0.price }.reversed(),
-                    asksQuanity:  orderbookData.bids.reversed()
-                        .map {
-                            Quantity(
-                                text: $0.quantity,
-                                rate: self?.calculateRateOfQuintity(
-                                    quantities: orderbookData.asks.map{ $0.quantity },
-                                    quantity: $0.quantity
-                                ) ?? 0
-                            )
-                        },
-                    bidsQuanity: orderbookData.asks.reversed()
-                        .map {
-                            Quantity(
-                                text: $0.quantity,
-                                rate: self?.calculateRateOfQuintity(
-                                    quantities: orderbookData.bids.map{ $0.quantity },
-                                    quantity: $0.quantity
-                                ) ?? 0
-                            )
-                        }
-                )
-            case .failure(let failure):
-                //TODO: fail 처리
-                print(failure)
-            }
-        }
     }
     
 }
