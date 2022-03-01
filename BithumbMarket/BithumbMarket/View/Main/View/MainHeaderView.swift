67c08ae120c6a4600149b56fc80af1bb22c8aafe
HOONHA CHOI <sosqsz2@naver.com>
2022-03-02 04:45:05 +0900

    [#20] feat: HeaderView Configure 구성

--- BithumbMarket/BithumbMarket/View/Main/View/MainHeaderView.swift
+++ BithumbMarket/BithumbMarket/View/Main/View/MainHeaderView.swift
@@ -18,10 +18,48 @@ final class MainHeaderView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        configureLabels()
+        configureStackView()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        configureLabels()
+        configureStackView()
+    }
+    
+    func configureLabels() {
+        [symbolsTitleLabel, currentPriceTitleLabel, fluctuateTitleLabel, accTradeValueTitleLabel].forEach { label in
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.font = .preferredFont(forTextStyle: .caption1)
+            label.textColor = .typoColor
+        }
+                
+        symbolsTitleLabel.text = "가산자산명"
+        currentPriceTitleLabel.text = "현재가"
+        fluctuateTitleLabel.text = "변동률"
+        accTradeValueTitleLabel.text = "거래금액"
+    }
+    
+    func configureStackView() {
+        titleStackView.translatesAutoresizingMaskIntoConstraints = false
+        titleStackView.axis = .horizontal
+        titleStackView.distribution = .equalCentering
+        titleStackView.alignment = .fill
+        titleStackView.spacing = 0
+        
+        [symbolsTitleLabel, currentPriceTitleLabel, fluctuateTitleLabel, accTradeValueTitleLabel].forEach { label in
+            titleStackView.addArrangedSubview(label)
+        }
+        
+        addSubview(titleStackView)
+        
+        NSLayoutConstraint.activate([
+            titleStackView.topAnchor.constraint(equalTo: topAnchor),
+            titleStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            titleStackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+            titleStackView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
     }
     
 }
