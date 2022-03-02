bc13a98018c8a9bb871d039e216d33ebef720a9a
HOONHA CHOI <sosqsz2@naver.com>
2022-03-02 15:55:40 +0900

    [#22] feat: TransactionPrice 그래프, 시간선택 뷰 추가

---
+++ BithumbMarket/BithumbMarket/View/Detail/View/TransactionPriceGraphView.swift
@@ -0,0 +1,13 @@
+//
+//  TransactionPriceGraphView.swift
+//  BithumbMarket
+//
+//  Created by HOONHA CHOI on 2022/03/02.
+//
+
+import UIKit
+
+final class TransactionPriceGraphView: UIView {
+    
+}
+
---
+++ BithumbMarket/BithumbMarket/View/Detail/View/TransactionPriceSelectTimeView.swift
@@ -0,0 +1,12 @@
+//
+//  TransactionPriceSelectTimeView.swift
+//  BithumbMarket
+//
+//  Created by HOONHA CHOI on 2022/03/02.
+//
+
+import UIKit
+
+final class TransactionPriceSelectTimeView: UIView {
+    
+}
