0e810a10200bf6dde90a24fddb72d9e887ddac38
JIINHEO <gjwldls123@gmail.com>
2022-03-02 10:23:08 +0900

    [#15] feat: Socket 연결

--- BithumbMarket/BithumbMarket/View/Transaction/TransactionViewModel.swift
+++ BithumbMarket/BithumbMarket/View/Transaction/TransactionViewModel.swift
@@ -26,7 +26,40 @@ final class TransactionViewModel {
             case .success(let transaction):
                 self.transactionData.value = transaction.data.reversed()
                 self.updateTableHandler?()
+                self.sendMessage()
+                self.updateTransaction()
             case.failure(let error):
+                self.errorHandler?(error)
+            }
+        }
+    }
+    
+    private func sendMessage() {
+        DispatchQueue.global().asyncAfter(deadline: .now() + 1) { [weak self] in
+            guard let self = self else { return }
+            let message = Message(type: .transaction, symbols: .name(self.name))
+            self.service.sendSocketMessage(to: message)
+        }
+    }
+    
+    func updateTransaction() {
+        service.perform { [weak self] (result: Result<ReceiveTransaction, HTTPError>) in
+            guard let self = self else { return }
+            switch result {
+            case .success(let transaction):
+                if let data = transaction.content.list.first {
+                    let transactionData = TransactionData(
+                        transactionDate: data.contDtm,
+                        type: data.buySellGb,
+                        unitsTraded: data.contQty,
+                        price: data.contPrice,
+                        total: data.contAmt
+                    )
+                    print(transactionData)
+                    self.transactionData.value.insert(transactionData, at: 0)
+                    self.insertTableHandler?()
+                }
+            case .failure(let error):
                 self.errorHandler?(error)
             }
         }
