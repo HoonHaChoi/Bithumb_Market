26aadb38d0543e0ce893f6433b5b4e437ea0af31
HOONHA CHOI <sosqsz2@naver.com>
2022-03-02 14:48:09 +0900

    [#22] feat: CurrentMarketPriceView,DetailViewConroller 추가

---
+++ BithumbMarket/BithumbMarket/View/Detail/DetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.swift
+//  BithumbMarket
+//
+//  Created by HOONHA CHOI on 2022/03/02.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
---
+++ BithumbMarket/BithumbMarket/View/Detail/View/CurrentMarketPriceView.swift
@@ -0,0 +1,22 @@
+//
+//  CurrentMarketPriceView.swift
+//  BithumbMarket
+//
+//  Created by HOONHA CHOI on 2022/03/02.
+//
+
+import UIKit
+
+final class CurrentMarketPriceView: UIView {
+    
+    private let currentPriceLabel = UILabel()
+    private let changeRateLabel = UILabel()
+    private let changePriceLabel = UILabel()
+    private let orderBookButton = UIButton()
+    
+    private let currentMarketPriceStackView = UIStackView()
+    private let currnetPriceStackView = UIStackView()
+    private let changeStackView = UIStackView()
+    
+    
+}
