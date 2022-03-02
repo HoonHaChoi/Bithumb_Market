e23d883e728735b5b36c5d77ad93129e8d1cf5be
Doku <ido.young@icloud.com>
2022-03-02 23:08:39 +0900

    [#13] feat: OrderbookDataSource final 적용

--- BithumbMarket/BithumbMarket/View/Orderbook/OrderbookDataSource.swift
+++ BithumbMarket/BithumbMarket/View/Orderbook/OrderbookDataSource.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class OrderbookDataSource: NSObject, UITableViewDataSource {
+final class OrderbookDataSource: NSObject, UITableViewDataSource {
     
     var items: Orderbook
     
