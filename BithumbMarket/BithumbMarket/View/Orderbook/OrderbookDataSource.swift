af3d5351988417dc084330d759295cfce93b9dbe
Doku <ido.young@icloud.com>
2022-03-02 10:30:00 +0900

    [#13] feat: Orderbook DataSource 추가

---
+++ BithumbMarket/BithumbMarket/View/Orderbook/OrderbookDataSource.swift
@@ -0,0 +1,59 @@
+//
+//  OrderbookTableViewDataSource.swift
+//  BithumbMarket
+//
+//  Created by Doyoung on 2022/03/02.
+//
+
+import UIKit
+
+class OrderbookDataSource: NSObject, UITableViewDataSource {
+    
+    var items: Orderbook
+    
+    enum OrderType: CaseIterable {
+        case ask
+        case bid
+        
+        var section: Int {
+            switch self {
+            case .ask:
+                return 0
+            case .bid:
+                return 1
+            }
+        }
+    }
+    
+    init(items: Orderbook) {
+        self.items = items
+        super.init()
+    }
+    
+    func updateCellData(by orderbook: Orderbook) {
+        self.items = orderbook
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return OrderType.allCases.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case OrderType.ask.section:
+            return items.asks.count
+        case OrderType.bid.section:
+            return items.bids.count
+        default:
+            return Int.zero
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: OrderbookNameSpace.cellReuseIdentifier, for: indexPath) as? OrderbookTableViewCell else {
+            return UITableViewCell()
+        }
+        return cell
+    }
+    
+}
