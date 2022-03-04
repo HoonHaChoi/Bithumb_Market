245221b1969a09d2dcb011dd23f07a0fa78537ef
HOONHA CHOI <sosqsz2@naver.com>
2022-03-05 01:16:17 +0900

    [#34] feat: LikeStorage 조회,저장,삭제,찾기 구현

---
+++ BithumbMarket/BithumbMarket/Storage/CoreDataError.swift
@@ -0,0 +1,8 @@
+//
+//  CoreDataError.swift
+//  BithumbMarket
+//
+//  Created by HOONHA CHOI on 2022/03/05.
+//
+
+import Foundation
--- BithumbMarket/BithumbMarket/Storage/LikeStorage.swift
+++ BithumbMarket/BithumbMarket/Storage/LikeStorage.swift
@@ -15,14 +15,71 @@ final class LikeStorge {
         let container = NSPersistentContainer(name: storageKey)
         container.loadPersistentStores { _, error in
             if let error = error {
-                fatalError()
+                fatalError("스토어를 로드하지 못했습니다 \(error)")
             }
         }
         return container
     }()
     
-    var context: NSManagedObjectContext {
+    private var context: NSManagedObjectContext {
         return persistentContainer.viewContext
     }
     
+    func fetch() -> Result<[Like], CoreDataError> {
+        guard let likes = try? context.fetch(Like.fetchRequest()) else {
+            return .failure(.failureFetch)
+        }
+        return .success(likes)
+    }
+    
+    func save(symbol: String) -> Result<Bool, CoreDataError> {
+        let like = Like(context: context)
+        like.symbol = symbol
+        
+        do {
+            try context.save()
+            return .success(true)
+        } catch {
+            return .failure(.failureSave)
+        }
+    }
+    
+    func delete(symbol: String) -> Result<Bool, CoreDataError> {
+        let request = Like.fetchRequest()
+        request.predicate = NSPredicate(format: "symbol == %@", symbol)
+        
+        do {
+            let likes = try context.fetch(request)
+            if let like = likes.first {
+                context.delete(like)
+                try context.save()
+            } else {
+                return .failure(.failureDelete)
+            }
+            return .success(true)
+        } catch {
+            return .failure(.failureDelete)
+        }
+    }
+    
+    func find(symbol: String) -> Result<Bool, CoreDataError> {
+        let request = Like.fetchRequest()
+        request.predicate = NSPredicate(format: "symbol == %@", symbol)
+        
+        do {
+            let likes = try context.fetch(request)
+            return .success(!likes.isEmpty)
+        } catch {
+            return .failure(.failiureFind)
+        }
+    }
+    
 }
+
+enum CoreDataError: Error {
+    case failureFetch
+    case failureSave
+    case failureDelete
+    case failiureFind
+}
+
