74da05873f3db602cc306cb8dd87b7daf5a9562b
JIINHEO <gjwldls123@gmail.com>
2022-03-04 11:08:51 +0900

    [#27] feat: CandleStick Model 추가

---
+++ BithumbMarket/BithumbMarket/Model/CandleStick.swift
@@ -0,0 +1,38 @@
+//
+//  CandleStick.swift
+//  BithumbMarket
+//
+//  Created by jiinheo on 2022/03/03.
+//
+
+import Foundation
+
+struct CandleStick: Decodable {
+    let status: String
+    let data: [GraphData]
+}
+
+struct GraphData: Decodable {
+    let time: Double
+    let openPrice: String
+    let closPrice: String
+    let minPrice: String
+    let maxPrice: String
+    let unitsTraded: String
+    
+    var date: Date {
+        return Date(timeIntervalSince1970: time / 1000 )
+    }
+}
+
+extension GraphData {
+    init(from decoder: Decoder) throws {
+        var container = try decoder.unkeyedContainer()
+        self.time = try container.decode(Double.self)
+        self.openPrice = try container.decode(String.self)
+        self.closPrice = try container.decode(String.self)
+        self.minPrice = try container.decode(String.self)
+        self.maxPrice = try container.decode(String.self)
+        self.unitsTraded = try container.decode(String.self)
+    }
+}
