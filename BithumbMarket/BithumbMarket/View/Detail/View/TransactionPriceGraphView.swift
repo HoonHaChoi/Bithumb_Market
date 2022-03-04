5623bccc65e8202f9c0db8aea20d9b42583350c6
JIINHEO <gjwldls123@gmail.com>
2022-03-04 11:22:36 +0900

    [#27] feat: TransactionPriceGraphView 구현

--- BithumbMarket/BithumbMarket/View/Detail/View/TransactionPriceGraphView.swift
+++ BithumbMarket/BithumbMarket/View/Detail/View/TransactionPriceGraphView.swift
@@ -9,5 +9,47 @@ import UIKit
 
 final class TransactionPriceGraphView: UIView {
     
+    let viewmodel = GraphViewModel()
+    var graph: Graph = Graph()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+      
+        viewmodel.fetchGraphPrice() {
+            DispatchQueue.main.async {
+                self.drawgraph()
+                self.setupView()
+            }
+        }
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        drawgraph()
+        setupView()
+    }
+    
+    private func drawgraph() {
+        graph = Graph(
+            frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 300),
+            values: self.viewmodel.price,
+            color: UIColor.mainColor.cgColor)
+    }
+    
 }
 
+extension TransactionPriceGraphView {
+    
+    private func setupView() {
+        addSubview(graph)
+        graph.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            graph.topAnchor.constraint(equalTo: self.topAnchor),
+            graph.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            graph.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            graph.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
+    
+}
