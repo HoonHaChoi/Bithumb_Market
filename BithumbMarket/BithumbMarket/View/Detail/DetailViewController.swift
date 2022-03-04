ea097be00417357ed9b3895e475d2232766c2b72
JIINHEO <gjwldls123@gmail.com>
2022-03-04 11:12:30 +0900

    [#27] style: TransactionPriceGraphView 추가

--- BithumbMarket/BithumbMarket/View/Detail/DetailViewController.swift
+++ BithumbMarket/BithumbMarket/View/Detail/DetailViewController.swift
@@ -15,6 +15,12 @@ class DetailViewController: UIViewController {
         return view
     }()
     
+    let transactionPriceGraphView: TransactionPriceGraphView = {
+        let view = TransactionPriceGraphView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     let transactionPriceSelectTimeView: TransactionPriceSelectTimeView = {
         let view = TransactionPriceSelectTimeView()
         view.translatesAutoresizingMaskIntoConstraints = false
@@ -38,16 +44,22 @@ class DetailViewController: UIViewController {
         
         view.backgroundColor = .systemBackground
         view.addSubview(currentMarketPriceView)
+        view.addSubview(transactionPriceGraphView)
         view.addSubview(transactionPriceSelectTimeView)
         view.addSubview(transactionHistoryView)
         view.addSubview(assetsStatusView)
         
         NSLayoutConstraint.activate([
-            currentMarketPriceView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            currentMarketPriceView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
             currentMarketPriceView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             currentMarketPriceView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
             
-            transactionPriceSelectTimeView.topAnchor.constraint(equalTo: currentMarketPriceView.bottomAnchor, constant: 20),
+            transactionPriceGraphView.topAnchor.constraint(equalTo: currentMarketPriceView.bottomAnchor, constant: 20),
+            transactionPriceGraphView.leadingAnchor.constraint(equalTo: currentMarketPriceView.leadingAnchor),
+            transactionPriceGraphView.trailingAnchor.constraint(equalTo: currentMarketPriceView.trailingAnchor),
+            transactionPriceGraphView.heightAnchor.constraint(equalToConstant: 300),
+            
+            transactionPriceSelectTimeView.topAnchor.constraint(equalTo: transactionPriceGraphView.bottomAnchor, constant: 20),
             transactionPriceSelectTimeView.leadingAnchor.constraint(equalTo: currentMarketPriceView.leadingAnchor),
             transactionPriceSelectTimeView.trailingAnchor.constraint(equalTo: currentMarketPriceView.trailingAnchor),
             
