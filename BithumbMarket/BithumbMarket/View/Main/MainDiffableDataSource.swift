aeddabbb80084955928bb7e8cc590b77f4249a61
HOONHA CHOI <sosqsz2@naver.com>
2022-03-06 23:34:36 +0900

    [#37] feat: MainDiffableDataSoure 추가

---
+++ BithumbMarket/BithumbMarket/View/Main/MainDiffableDataSource.swift
@@ -0,0 +1,46 @@
+//
+//  MainDiffableDataSource.swift
+//  BithumbMarket
+//
+//  Created by HOONHA CHOI on 2022/03/06.
+//
+
+import UIKit
+
+enum Section: Hashable {
+    case main
+}
+
+final class MainDiffableDataSource: UITableViewDiffableDataSource<Section, Ticker> {
+    
+    private var items: [Ticker] = []
+    
+    func updateItems(tickers: [Ticker]) {
+        items = tickers
+    }
+    
+    func appendSnapshot(tickers: [Ticker]) {
+        self.items = tickers
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Ticker>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(items)
+        apply(snapshot: snapshot)
+    }
+    
+    func reloadSnapshot(ticker: Ticker, completion: (() -> Void)?) {
+        var currentSnapshot = self.snapshot()
+        currentSnapshot.reloadItems([ticker])
+        apply(snapshot: currentSnapshot, completion: completion)
+    }
+    
+    private func apply(snapshot: NSDiffableDataSourceSnapshot<Section, Ticker>, completion: (() -> Void)? = nil) {
+        DispatchQueue.main.async {
+            self.apply(snapshot, animatingDifferences: false, completion: completion)
+        }
+    }
+    
+    func isEmptyItems() -> Bool {
+        items.isEmpty
+    }
+    
+}
