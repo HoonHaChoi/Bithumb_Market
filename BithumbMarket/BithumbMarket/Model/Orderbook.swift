63a8e351a26b14f12da86b91b1de683f2764fd91
Doku <ido.young@icloud.com>
2022-03-07 13:14:56 +0900

    [#31] refactor: Orderbook Model 수정

--- BithumbMarket/BithumbMarket/Model/Orderbook.swift
+++ BithumbMarket/BithumbMarket/Model/Orderbook.swift
@@ -2,18 +2,45 @@
 //  Orderbook.swift
 //  BithumbMarket
 //
-//  Created by Doyoung on 2022/02/28.
+//  Created by Doyoung on 2022/02/25.
 //
 
 import Foundation
 
-struct Orderbook {
-    var asks: [Order]
-    var bids: [Order]
+struct Orderbook: Decodable {
+    let data: OrderbookData
 }
 
-struct Order {
-    let price: String
+struct OrderbookData: Decodable {
+    var asks: [Order]
+    var bids: [Order]
+    
+    func calculateRateOfAsks() -> [Float] {
+        return calculateRateOfQuintity(orders: asks)
+    }
+    
+    func calculateRateOfBids() -> [Float] {
+        return calculateRateOfQuintity(orders: bids)
+    }
+}
+
+extension OrderbookData {
+    
+    func calculateRateOfQuintity(orders: [Order]) -> [Float] {
+        var rate = [Float]()
+        let quantities = orders
+            .map { Float($0.quantity) ?? 0 }
+        let sum = quantities
+            .reduce(0) { $0 + $1 }
+        quantities.forEach {
+            rate.append(round($0 / sum * 5 * 100) / 100)
+        }
+        return rate
+    }
+    
+}
+
+struct Order: Decodable {
     let quantity: String
-    let rateOfQuantity: Float
+    let price: String
 }
