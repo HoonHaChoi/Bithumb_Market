b0ccb2a48bedada2de1f4127e000a9ca38fab4a3
Doku <ido.young@icloud.com>
2022-03-07 13:41:41 +0900

    [#31] refactor: OrderbookViewModel 수정

--- BithumbMarket/BithumbMarket/View/Orderbook/OrderbookViewModel.swift
+++ BithumbMarket/BithumbMarket/View/Orderbook/OrderbookViewModel.swift
@@ -10,33 +10,18 @@ import Foundation
 final class OrderbookViewModel {
 
     var symbol: String
-    var orderbook: Observable<Orderbook>
+    var orderbook: Observable<OrderbookData>
     private var service: APIService
     
     init(service: APIService = APIService(), symbol: String) {
-        self.orderbook =  Observable(
-            Orderbook(
-                asks: Array(
-                    repeating: Order(
-                        price: OrderbookNameSpace.emptyPrice,
-                        quantity: OrderbookNameSpace.noQuantity,
-                        rateOfQuantity: 0),
-                    count: 30),
-                bids:  Array(
-                    repeating: Order(
-                        price: OrderbookNameSpace.emptyPrice,
-                        quantity: OrderbookNameSpace.noQuantity,
-                        rateOfQuantity: 0),
-                    count: 30)
-            )
-        )
+        self.orderbook = .init(OrderbookData(
+            asks: Array(repeating: .init(quantity: "", price: ""), count: 30),
+            bids: Array(repeating: .init(quantity: "", price: ""), count: 30)))
         self.service = service
         self.symbol = symbol
     }
     
-    private var orderbookData: OrderbookData?
     var errorHandler: ((HTTPError) -> Void)?
-    var updateTableHandler: (() -> Void)?
     
     func featchOrderbook(completion: @escaping () -> Void) {
         completion()
@@ -46,17 +31,10 @@ final class OrderbookViewModel {
     }
     
     private func requestOrderbook(completion: @escaping () -> Void) {
-        service.request(endpoint: .orderBook(symbol: symbol)) { [weak self] (result: Result<OrderbookEntity, HTTPError>) in
+        service.request(endpoint: .orderBook(symbol: symbol)) { [weak self] (result: Result<Orderbook, HTTPError>) in
             switch result {
             case .success(let success):
-                let orderbook = success.data
-                self?.orderbookData = orderbook
-                guard let asks = self?.convert(from: orderbook.asks),
-                      let bids = self?.convert(from: orderbook.bids) else {
-                          return
-                      }
-                self?.orderbook.value = Orderbook(asks: asks, bids: bids)
-                self?.updateTableHandler?()
+                self?.orderbook.value = success.data
                 self?.sendMessage()
                 DispatchQueue.main.async {
                     completion()
@@ -79,31 +57,23 @@ final class OrderbookViewModel {
         self.service.perform { [weak self] (result: Result<ReceiveOrderbook, HTTPError>) in
             switch result {
             case .success(let success):
-                guard let orderbookData = self?.orderbookData else {
-                    return }
+                guard let self = self else { return }
                 let receivedOrderbook = success.content.list
                 let receivedAsks = receivedOrderbook.filter { $0.orderType == OrderbookNameSpace.ask }
                 let receivedBids = receivedOrderbook.filter { $0.orderType == OrderbookNameSpace.bid }
-                guard let asksEntity = self?.mergeOrders(orderbookData.asks, into: receivedAsks),
-                      let bidsEntity = self?.mergeOrders(orderbookData.bids, into: receivedBids) else {
-                          return
-                      }
-                guard let asks = self?.convert(from: asksEntity).suffix(30),
-                      let bids = self?.convert(from: bidsEntity).prefix(30) else {
-                          return
-                      }
-                self?.orderbook.value = Orderbook(
-                    asks: Array(asks),
-                    bids: Array(bids)
-                )
-                self?.updateTableHandler?()
+                
+                let asks = Array(self.mergeOrders(self.orderbook.value.asks, into: receivedAsks).prefix(30))
+                let bids = Array(self.mergeOrders(self.orderbook.value.bids, into: receivedBids).suffix(30))
+                
+                self.orderbook.value = .init(asks: asks,
+                                             bids: bids)
             case .failure(let error):
                 self?.errorHandler?(error)
             }
         }
     }
     
-    private func mergeOrders(_ old: [OrderEntity], into new: [ReceiveOrder]) -> [OrderEntity] {
+    private func mergeOrders(_ old: [Order], into new: [ReceiveOrder]) -> [Order] {
         var order = old
         let newOrderPrice = new.map { $0.price }
         let previousOrderPrice = old.map { $0.price }
@@ -118,14 +88,14 @@ final class OrderbookViewModel {
                 order.remove(at: samePrice)
                 if new[index].quantity != OrderbookNameSpace.noQuantity {
                     order.append(
-                        OrderEntity(
+                        Order(
                             quantity: new[index].quantity,
                             price: new[index].price)
                     )
                 }
             } else if new[index].quantity != OrderbookNameSpace.noQuantity {
                 order.append(
-                    OrderEntity(
+                    Order(
                         quantity: new[index].quantity,
                         price: new[index].price)
                 )
@@ -134,25 +104,6 @@ final class OrderbookViewModel {
         order.sort{ Double($0.price) ?? 0 > Double($1.price) ?? 0 }
         
         return order
-    }
-    
-    private func convert(from entity: [OrderEntity]) -> [Order]{
-        return entity
-            .map {
-                Order(
-                    price: $0.price.withComma(),
-                    quantity: $0.quantity.withDecimal(maximumDigit: 4),
-                    rateOfQuantity: self.calculateRateOfQuintity(
-                        quantities: entity.map{ $0.quantity },
-                        quantity: $0.quantity))
-            }
-    }
-    
-    private func calculateRateOfQuintity(quantities: [String], quantity: String) -> Float {
-        let quantities = quantities.map { Float($0) ?? 0 }
-        let sumOfQuantities = quantities.reduce(0){ $0 + $1}
-        let rate = Float(quantity) ?? 0 / sumOfQuantities * 5
-        return rate > 1 ? 1 : rate
     }
     
 }
