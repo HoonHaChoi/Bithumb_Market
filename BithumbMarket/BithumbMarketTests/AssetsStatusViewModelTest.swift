29fa2bbe1988a2682c9a755873a13bafbc2f7391
HOONHA CHOI <sosqsz2@naver.com>
2022-03-12 05:17:37 +0900

    [#63] feat: AssetsStatus 입출금 테스트 추가

--- BithumbMarket/BithumbMarketTests/AssetsStatusViewModelTest.swift
+++ BithumbMarket/BithumbMarketTests/AssetsStatusViewModelTest.swift
@@ -8,21 +8,57 @@
 import XCTest
 
 class AssetsStatusViewModelTest: XCTestCase {
-
-    var assetsStatusViewModel: AssetsStatusViewModel!
-    var service: Serviceable!
     
-    override func setUpWithError() throws {
-        service = MockAPIService()
-        assetsStatusViewModel = AssetsStatusViewModel(service: service, symbol: "")
+    override func setUpWithError() throws {}
+
+    override func tearDownWithError() throws {}
+
+    func test_입출금_불가() throws {
+        let service = MockAssetsStatusAPIService(isSuccess: true, index: 0)
+        let viewmodel = AssetsStatusViewModel(service: service, symbol: "")
+        
+        let status: ((AssetsState) -> Void)? = {
+            XCTAssertEqual($0, .impossible)
+        }
+        
+        viewmodel.assetsStateHandler = status
+        viewmodel.fetchAssetsStatus()
+    }
+    
+    func test_입금불가_출금가능() throws {
+        let service = MockAssetsStatusAPIService(isSuccess: true, index: 1)
+        let viewmodel = AssetsStatusViewModel(service: service, symbol: "")
+        
+        let status: ((AssetsState) -> Void)? = {
+            XCTAssertEqual($0, .possibleWithdrawal)
+        }
+        
+        viewmodel.assetsStateHandler = status
+        viewmodel.fetchAssetsStatus()
     }
 
-    override func tearDownWithError() throws {
-        service = nil
-        assetsStatusViewModel = nil
+    func test_입금가능_출금불가() throws {
+        let service = MockAssetsStatusAPIService(isSuccess: true, index: 2)
+        let viewmodel = AssetsStatusViewModel(service: service, symbol: "")
+        
+        let status: ((AssetsState) -> Void)? = {
+            XCTAssertEqual($0, .possibleDeposit)
+        }
+        
+        viewmodel.assetsStateHandler = status
+        viewmodel.fetchAssetsStatus()
     }
-
-    func testExample() throws {
+    
+    func test_입출금가능() throws {
+        let service = MockAssetsStatusAPIService(isSuccess: true, index: 3)
+        let viewmodel = AssetsStatusViewModel(service: service, symbol: "")
+        
+        let status: ((AssetsState) -> Void)? = {
+            XCTAssertEqual($0, .possibleAll)
+        }
+        
+        viewmodel.assetsStateHandler = status
+        viewmodel.fetchAssetsStatus()
     }
-
+    
 }
