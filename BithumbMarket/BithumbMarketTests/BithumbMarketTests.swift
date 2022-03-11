cdeafa117118c67b17f8c67160c42c9a639baf94
HOONHA CHOI <sosqsz2@naver.com>
2022-03-12 02:48:16 +0900

    [#61] feat: 네트워크 성공과 실패 시 데이터 테스트

--- BithumbMarket/BithumbMarketTests/BithumbMarketTests.swift
+++ BithumbMarket/BithumbMarketTests/BithumbMarketTests.swift
@@ -8,19 +8,43 @@
 import XCTest
 @testable import BithumbMarket
 
-class BithumbMarketTests: XCTestCase {
-
+class TransactionViewModelTests: XCTestCase {
+    
+    var transactionViewModel: TransactionViewModel!
+    var service: Serviceable!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        service = MockAPIService()
+        transactionViewModel = TransactionViewModel(service: service, symbol: "")
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        service = nil
+        transactionViewModel = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test_체결내역_성공적으로_가져오기() throws {
+        transactionViewModel.fetchTransaction()
+        
+        let expectedTransactionDataType = transactionViewModel.transactionData.value[0].type
+        let expectedTransactionDataPrice = transactionViewModel.transactionData.value[0].price
+        let expectedTransactionDataTotal = transactionViewModel.transactionData.value[1].total
+        
+        let expectTransactionType = "ask"
+        let expectTransactionPrice = "1"
+        let expectTransactionTotal = "1"
+        
+        XCTAssertEqual(expectedTransactionDataType, expectTransactionType)
+        XCTAssertEqual(expectedTransactionDataPrice, expectTransactionPrice)
+        XCTAssertEqual(expectedTransactionDataTotal, expectTransactionTotal)
+    }
+    
+    func test_채결내역_요청실패() throws {
+        service = MockAPIService(isSuccess: false)
+        transactionViewModel = TransactionViewModel(service: service, symbol: "")
+        transactionViewModel.fetchTransaction()
+        
+        XCTAssertTrue(transactionViewModel.transactionData.value.isEmpty)
     }
 
 }
